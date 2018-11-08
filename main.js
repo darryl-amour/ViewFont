1dca560c4ddb349200bc754f4832101aa869513c
Darryl Amour <darryl.amour@gmail.com>
2018-11-07 20:50:48 -0800

    Initial structure of main.js where main functionality will reside

---
+++ main.js
@@ -0,0 +1,53 @@
+/*
+ *
+ */
+function ViewFont() {}
+
+/*
+ *
+ */
+ViewFont.prototype.findInPage = function findInPage() {
+
+};
+
+/*
+ *
+*/
+ViewFont.prototype.copyToClipBoard = function copyToClipBoard() {
+
+};
+
+/*
+ *
+ */
+ViewFont.prototype.popUpWindow = function popUpWindow() {
+
+};
+
+/*
+ *
+ */
+ViewFont.prototype.identifyElements = function identifyElements() {
+
+};
+
+/*
+ *
+ */
+ViewFont.prototype.displayFont = function displayFont() {
+
+};
+
+/*
+ *
+ */
+ViewFont.prototype.enable = function enable() {
+
+};
+
+/*
+ *
+ */
+ViewFont.prototype.disable = function disable() {
+
+};
